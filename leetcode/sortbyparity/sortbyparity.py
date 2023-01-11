6df7bc508812d2b546b05978da387e4a9b7e07a1
Joe Swantek <joseph.swantek@northwestern.edu>
2023-01-10 21:40:30 -0600

    leetcode: modify sort by parity challenge (python) - fast solution
    While still O(n) time complexity, it is faster as the array is only
    looped 1 time.  Howeever, requires additional memory (O(n) space complexity).

--- leetcode/sortbyparity/sortbyparity.py
+++ leetcode/sortbyparity/sortbyparity.py
@@ -1,7 +1,12 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        # Time: O(n) - but loops the array 2 times
-        # Space: O(1) (additional)
-        evens = [n for n in nums if n % 2 == 0]
-        odds = [n for n in nums if n % 2 != 0]
+        # Time: O(n) - but only loops the array 1 time
+        # Space: O(n) (additinal)
+        evens = []
+        odds = []
+        for n in nums:
+            if n % 2 == 0:
+                evens.append(n)
+            else:
+                odds.append(n)
         return evens + odds
