99f50cb724e15520168473479103fe63ee9f1bd8
Joe Swantek <joseph.swantek@northwestern.edu>
2023-01-27 10:50:16 -0600

    other [dupstring] fixed some typos and added test cases
    the algorithm from my original submission was correct

--- other/dupstring/dupstring.py
+++ other/dupstring/dupstring.py
@@ -1,14 +1,23 @@
-def func(input):
+def returndups(input):
     found = {}
     res = ""
     for c in input:
-        if not found[c]:
+        if not found.get(c):
             found[c] = 1
         else:
             found[c] = found[c] + 1
 
-    for k, v in found:
+    for k, v in found.items():
         if v > 1:
             res = res + k
 
     return res
+
+def main():
+    assert(returndups("joseph") == "")
+    assert(returndups("helloo") == "lo")
+    assert(returndups("hhheeellloo") == "helo")
+    assert(returndups("") == "")
+
+if __name__ == "__main__":
+    main()
